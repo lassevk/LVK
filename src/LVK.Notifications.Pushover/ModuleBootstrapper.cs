d3406c5c7b235f848dd53bdb20c86051d397873b
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2024-02-19 11:39:18 +0100

    LVK.Notifications.Pushover must bootstrap LVK.Events

--- src/LVK.Notifications.Pushover/ModuleBootstrapper.cs
+++ src/LVK.Notifications.Pushover/ModuleBootstrapper.cs
@@ -11,6 +11,7 @@ public class ModuleBootstrapper : IModuleBootstrapper
     public void Bootstrap(IHostBootstrapper bootstrapper, IHostApplicationBuilder builder)
     {
         bootstrapper.Bootstrap(new LVK.Networking.ModuleBootstrapper());
+        bootstrapper.Bootstrap(new LVK.Events.ModuleBootstrapper());
 
         builder.Services.AddSingleton<IEventSubscriber<PushoverNotification>, PushoverNotificationSubscriber>();
         builder.Services.Configure<PushoverNotificationOptions>(builder.Configuration.GetSection("Notifications:Pushover"));
