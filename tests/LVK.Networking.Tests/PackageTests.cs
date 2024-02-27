634df185795fbaa1440200183657e6acc8a06b69
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2024-02-27 09:38:55 +0100

    Add tests for new projects

---
+++ tests/LVK.Networking.Tests/PackageTests.cs
@@ -0,0 +1,10 @@
+using LVK.Tests;
+
+namespace LVK.Networking.Test;
+
+[TestFixture]
+[NugetProject("../../src/LVK.Networking/LVK.Networking.csproj")]
+public class PackageTests : NugetTests<PackageTests>
+{
+
+}
---
+++ tests/LVK.Notifications.Pushover.Tests/PackageTests.cs
@@ -0,0 +1,10 @@
+using LVK.Tests;
+
+namespace LVK.Notifications.Pushover.Tests;
+
+[TestFixture]
+[NugetProject("../../src/LVK.Notifications.Pushover/LVK.Notifications.Pushover.csproj")]
+public class PackageTests : NugetTests<PackageTests>
+{
+
+}
