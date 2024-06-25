68ed16044acc1a5fb837926a853443725970c10a
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2024-06-25 14:34:11 +0200

    Remove configuration file handling

--- src/LVK.Core/ModuleBootstrapper.cs
+++ src/LVK.Core/ModuleBootstrapper.cs
@@ -1,7 +1,5 @@
 using LVK.Core.Bootstrapping;
 
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.Json;
 using Microsoft.Extensions.Hosting;
 
 namespace LVK.Core;
@@ -10,19 +8,5 @@ public class ModuleBootstrapper : IModuleBootstrapper
 {
     public void Bootstrap(IHostBootstrapper bootstrapper, IHostApplicationBuilder builder)
     {
-        string? assemblyLocation = Path.GetDirectoryName(GetType().Assembly.Location);
-        if (assemblyLocation == null)
-            return;
-
-        string[] configurationFiles =
-        [
-            "appsettings.json", $"appsettings.{builder.Environment.EnvironmentName}.json", $"appsettings.{builder.Environment.EnvironmentName}.json",
-            $"appsettings.{Environment.MachineName.ToLowerInvariant()}.json", $"appsettings.{Environment.MachineName.ToLowerInvariant()}.{builder.Environment.EnvironmentName}.json"
-        ];
-
-        IConfigurationBuilder b = builder.Configuration.SetBasePath(assemblyLocation).AddJsonFile("appsettings.json");
-
-        foreach (string configurationFile in configurationFiles)
-            b = b.AddJsonFile(configurationFile, optional: true);
     }
 }
