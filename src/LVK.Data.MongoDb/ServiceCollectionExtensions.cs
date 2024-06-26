c7ae72fcd4059ece033123ed0d4f6ae680720156
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2024-06-26 10:32:25 +0200

    Fixed logging with MongoDb

--- src/LVK.Data.MongoDb/ServiceCollectionExtensions.cs
+++ src/LVK.Data.MongoDb/ServiceCollectionExtensions.cs
@@ -1,40 +1,51 @@
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 using MongoDB.Driver;
+using MongoDB.Driver.Core.Configuration;
 
 namespace LVK.Data.MongoDb;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddMongoClient(this IServiceCollection services, IMongoClient client) => services.AddSingleton<IMongoClient>(client);
-    public static IServiceCollection AddMongoClient(this IServiceCollection services, string connectionString) => services.AddSingleton<IMongoClient>(new MongoClient(connectionString));
+    public static IServiceCollection AddMongoClient(this IServiceCollection services, IMongoClient client) => services.AddSingleton(client);
 
-    public static IServiceCollection AddKeyedMongoClient(this IServiceCollection services, object? serviceKey, IMongoClient client) => services.AddKeyedSingleton<IMongoClient>(serviceKey, client);
+    public static IServiceCollection AddMongoClient(this IServiceCollection services, string connectionString)
+        => services.AddSingleton<IMongoClient>(serviceProvider => CreateMongoClient(connectionString, serviceProvider));
+
+    public static IServiceCollection AddKeyedMongoClient(this IServiceCollection services, object? serviceKey, IMongoClient client) => services.AddKeyedSingleton(serviceKey, client);
 
     public static IServiceCollection AddKeyedMongoClient(this IServiceCollection services, object? serviceKey, string? connectionString)
-        => services.AddKeyedSingleton<IMongoClient>(serviceKey, new MongoClient(connectionString));
+        => services.AddKeyedSingleton<IMongoClient>(serviceKey, (serviceProvider, _) => CreateMongoClient(connectionString, serviceProvider));
 
-    public static IServiceCollection AddMongoDatabase(this IServiceCollection services, IMongoDatabase database) => services.AddSingleton<IMongoDatabase>(database);
+    private static MongoClient CreateMongoClient(string? connectionString, IServiceProvider serviceProvider)
+    {
+        var settings = MongoClientSettings.FromConnectionString(connectionString);
+        settings.LoggingSettings = new LoggingSettings(serviceProvider.GetRequiredService<ILoggerFactory>());
+        return new MongoClient(settings);
+    }
+
+    public static IServiceCollection AddMongoDatabase(this IServiceCollection services, IMongoDatabase database) => services.AddSingleton(database);
 
     public static IServiceCollection AddMongoDatabase(this IServiceCollection services, string databaseName)
-        => services.AddSingleton<IMongoDatabase>(sp => sp.GetRequiredService<IMongoClient>().GetDatabase(databaseName));
+        => services.AddSingleton<IMongoDatabase>(serviceProvider => serviceProvider.GetRequiredService<IMongoClient>().GetDatabase(databaseName));
 
     public static IServiceCollection AddKeyedMongoDatabase(this IServiceCollection services, object? serviceKey, IMongoDatabase database)
-        => services.AddKeyedSingleton<IMongoDatabase>(serviceKey, database);
+        => services.AddKeyedSingleton(serviceKey, database);
 
     public static IServiceCollection AddKeyedMongoDatabase(this IServiceCollection services, object? serviceKey, string? databaseName)
-        => services.AddKeyedSingleton<IMongoDatabase>(serviceKey, (sp, sk) => sp.GetRequiredKeyedService<IMongoClient>(sk).GetDatabase(databaseName));
+        => services.AddKeyedSingleton<IMongoDatabase>(serviceKey, (serviceProvider, key) => serviceProvider.GetRequiredKeyedService<IMongoClient>(key).GetDatabase(databaseName));
 
     public static IServiceCollection AddMongoCollection<T>(this IServiceCollection services, string collectionName)
-        => services.AddSingleton<IMongoCollection<T>>(sp => sp.GetRequiredService<IMongoDatabase>().GetCollection<T>(collectionName));
+        => services.AddSingleton<IMongoCollection<T>>(serviceProvider => serviceProvider.GetRequiredService<IMongoDatabase>().GetCollection<T>(collectionName));
 
     public static IServiceCollection AddMongoCollection<T>(this IServiceCollection services)
-        => services.AddSingleton<IMongoCollection<T>>(sp => sp.GetRequiredService<IMongoDatabase>().GetCollection<T>());
+        => services.AddSingleton<IMongoCollection<T>>(serviceProvider => serviceProvider.GetRequiredService<IMongoDatabase>().GetCollection<T>());
 
     public static IServiceCollection AddKeyedMongoCollection<T>(this IServiceCollection services, object? serviceKey, string collectionName)
-        => services.AddKeyedSingleton<IMongoCollection<T>>(serviceKey, (sp, sk) => sp.GetRequiredKeyedService<IMongoDatabase>(sk).GetCollection<T>(collectionName));
+        => services.AddKeyedSingleton<IMongoCollection<T>>(serviceKey, (serviceProvider, key) => serviceProvider.GetRequiredKeyedService<IMongoDatabase>(key).GetCollection<T>(collectionName));
 
     public static IServiceCollection AddKeyedMongoCollection<T>(this IServiceCollection services, object? serviceKey)
         where T : MongoDbDocument
-        => services.AddKeyedSingleton<IMongoCollection<T>>(serviceKey, (sp, sk) => sp.GetRequiredKeyedService<IMongoDatabase>(sk).GetCollection<T>());
+        => services.AddKeyedSingleton<IMongoCollection<T>>(serviceKey, (serviceProvider, key) => serviceProvider.GetRequiredKeyedService<IMongoDatabase>(key).GetCollection<T>());
 }
