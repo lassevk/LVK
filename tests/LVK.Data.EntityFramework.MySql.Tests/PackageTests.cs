fc68b97714841a5c977862211c14b805ed7bf91b
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2024-11-22 10:32:15 +0100

    Fix warnings, remove MySql library

--- tests/LVK.Data.EntityFramework.MySql.Tests/PackageTests.cs
+++
@@ -1,7 +0,0 @@
-using LVK.Tests;
-
-namespace LVK.Data.EntityFramework.MySql.Tests;
-
-[TestFixture]
-[NugetProject("../../src/LVK.Data.EntityFramework.MySql/LVK.Data.EntityFramework.MySql.csproj")]
-public class PackageTests : NugetTests<PackageTests>;
